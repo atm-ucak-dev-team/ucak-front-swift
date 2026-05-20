e7c95e959bff9da0ab477eddedde09816da1cbd2
bomsiwor <bomsiwor@gmail.com>
2026-05-20 15:05:19 +0700

    feat: change project name

--- UcakFront/FollupTests/FollupTest.swift
+++ UcakFront/FollupTests/FollupTest.swift
@@ -1,13 +1,13 @@
 //
-//  UcakFrontTests.swift
-//  UcakFrontTests
+//  FollupTest.swift
+//  FollupTest
 //
 //  Created by ATMUCAK  on 17/05/26.
 //
 
 import XCTest
 
-final class UcakFrontTests: XCTestCase {
+final class FollupTest: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
