4da93856c6fb51bd572d875ac03503bf21541405
Eileen Anindya <eileenanindya@gmail.com>
2026-05-21 18:01:05 +0700

    fix: fixing summary list job component

--- FollupApp/FollupApp/Views/Components/SummaryListJobCardView.swift
+++ FollupApp/FollupApp/Views/Components/SummaryListJobCardView.swift
@@ -8,28 +8,29 @@
 import SwiftUI
 
 struct SummaryListJobCardView: View {
-    let jobs: [JobRowCardModel] = [
-        JobRowCardModel(jobName: "Approval", dateInfo: "Next follow up: 21/06/2026", ticketInfo: "Jira Ticket: ADA-002", status: .ongoing),
-        JobRowCardModel(jobName: "Approval Budget", dateInfo: "Next follow up: 30/06/2026", ticketInfo: "Jira Ticket: ADA-003", status: .replied),
-        JobRowCardModel(jobName: "Approval Kartini's Day", dateInfo: "Next follow up: 10/06/2026", ticketInfo: "Jira Ticket: ADA-001", status: .ongoing)
-    ]
+    var items: [SummaryListJobItem] = []
     
-    let items: [SummaryListJobItem] = [
-        SummaryListJobItem(icon: "checkmark", title: "Replied", count: 2),
-        SummaryListJobItem(icon: "hourglass", title: "Ongoing", count: 1),
-        SummaryListJobItem(icon: "trash", title: "Expired", count: 3)
-    ]
-    
+    init(items: [SummaryListJobItem]? = nil){
+        if let items = items, !items.isEmpty{
+            self.items = items
+        } else{
+            self.items = [
+                SummaryListJobItem(icon: "checkmark", title: "Replied", count: 0),
+                SummaryListJobItem(icon: "hourglass", title: "Ongoing", count: 0),
+                SummaryListJobItem(icon: "trash", title: "Expired", count: 0)
+            ]
+        }
+    }
     var body: some View {
-        HStack{
-            ForEach(Array(items.enumerated()), id: \.element.id) { index,item in
+        HStack {
+            ForEach(Array(items.enumerated()), id: \.element.id) { index, item in
                 if index > 0 {
                     Divider()
                         .frame(height: 90)
                 }
                 
                 Spacer()
-                VStack(spacing: 10){
+                VStack(spacing: 10) {
                     Image(systemName: item.icon)
                         .font(.system(size: 16, weight: .medium))
                     Text("\(item.count)")
@@ -40,12 +41,23 @@ struct SummaryListJobCardView: View {
                 Spacer()
             }
         }
+        .frame(maxWidth: .infinity, alignment: .leading)
         .padding(30)
         .background(Color.themeBackground)
         .cornerRadius(26)
     }
 }
 
-#Preview {
+#Preview("With Data"){
+    SummaryListJobCardView(
+        items: [
+            SummaryListJobItem(icon: "checkmark", title: "Replied", count: 2),
+            SummaryListJobItem(icon: "hourglass", title: "Ongoing", count: 1),
+            SummaryListJobItem(icon: "trash", title: "Expired", count: 3)
+        ]
+    )
+}
+
+#Preview("Empty"){
     SummaryListJobCardView()
 }
