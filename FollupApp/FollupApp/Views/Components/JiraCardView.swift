a6fd6d956d25b8d42a0cca0e6ef3afe013e5a0d8
Salwa Aisyah <2110512052@mahasiswa.upnvj.ac.id>
2026-05-21 18:47:49 +0700

    feat(component): Jira ticket card component

---
+++ FollupApp/FollupApp/Views/Components/JiraCardView.swift
@@ -0,0 +1,52 @@
+//
+//  JiraCardView.swift
+//  FollupApp
+//
+//  Created by Salwa Aisyah Adhani on 21/05/26.
+//
+
+import SwiftUI
+
+struct JiraCardView: View {
+    var item: [JiraStatusLabelModel] = []
+    
+    var body: some View {
+        ForEach (Array(item.enumerated()), id: \.element.id) {
+            index, jira in
+            VStack (alignment: .leading, spacing: 12) {
+                JiraStatusLabelView(status: .done)
+                
+                Text(jira.ticketName)
+                    .frame(width: 125, alignment: .leading)
+                    .multilineTextAlignment(.leading)
+                    .font(Font.subheadline.weight(.regular))
+                    .foregroundColor(.themeTypography)
+                //                .background(Color.blue)
+                
+                Text(jira.ticketNumber)
+                    .frame(width: 125, alignment: .leading)
+                    .multilineTextAlignment(.leading)
+                    .font(Font.caption.weight(.medium))
+                    .foregroundColor(Color.gray)
+                //                .background(Color.blue)
+            }
+            .frame(width: 145, alignment: .center)
+            .padding(12)
+            .background(Color.white)
+            .cornerRadius(10)
+        }
+        
+    }
+    
+}
+
+
+#Preview {
+    JiraCardView()
+}
+
+#Preview ("With Data") {
+    JiraCardView(item: [
+        JiraStatusLabelModel(status: .done, ticketName: "Bikin Komponen Card", ticketNumber: "ADA-001")
+    ])
+}
