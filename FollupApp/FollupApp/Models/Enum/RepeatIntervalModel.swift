300b71d12bbd21f2601c2f44dfddf7cb1bbc312e
eileenanindya <125994138+eileenanindya@users.noreply.github.com>
2026-05-24 23:50:30 +0700

    feat: init follow up setup form screen (#49)

---
+++ FollupApp/FollupApp/Models/Enum/RepeatIntervalModel.swift
@@ -0,0 +1,14 @@
+//
+//  RepeatIntervalModel.swift
+//  FollupApp
+//
+//  Created by Eileen Anindya on 24/05/26.
+//
+
+enum RepeatInterval: String, Codable, CaseIterable{
+    case daily = "Daily"
+    case every2Days = "Every 2 Days"
+    case every3Days = "Every 3 Days"
+    case weekly = "Weekly"
+    case biweekly = "Biweekly"
+}
